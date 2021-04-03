15a9a6fc771dfdb2b40a4437210e9b82f96bad30
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-04-03 13:33:06 -0700

    2.3 Improvement - store patron ids in separate file

--- Commands/mydata.js
+++ Commands/mydata.js
@@ -2,6 +2,7 @@ const Command = require('./../Classes/Command.js');
 const { getString } = require('./../Localizations/localization.js');
 const { MessageEmbed } = require('discord.js');
 const { directories } = require('../helpers.js');
+const patrons = require('./../patrons.json');
 
 var command = new Command("mydata", false, false, false);
 
@@ -18,24 +19,18 @@ command.execute = (receivedMessage, state, locale) => {
 	Object.keys(dictionary).forEach(platform => {
 		text += `\n${platform}: ${dictionary[platform].value ? dictionary[platform].value : ''}`;
 	})
-	let observers = [];
-	let dreamers = [];
-	let explorers = ['103335856479162368'];
-	let cartographers = ['115221369339379720'];
-	let archivists = []
-	let grandArchivists = ['694403611979153488', '106122478715150336', '112785244733628416'];
 
-	if (observers.includes(receivedMessage.author.id)) { // Observer
+	if (patrons.observers.includes(receivedMessage.author.id)) { // Observer
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114192013000816/IH_PatreonTierImages_Observer.jpg');
-	} else if (dreamers.includes(receivedMessage.author.id)) { // Dreamer
+	} else if (patrons.dreamers.includes(receivedMessage.author.id)) { // Dreamer
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114186811932713/IH_PatreonTierImages_Dreamer.jpg');
-	} else if (explorers.includes(receivedMessage.author.id)) { // Explorer
+	} else if (patrons.explorers.includes(receivedMessage.author.id)) { // Explorer
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114188095651840/IH_PatreonTierImages_Explorer.jpg');
-	} else if (cartographers.includes(receivedMessage.author.id)) { // Cartographer
+	} else if (patrons.cartographers.includes(receivedMessage.author.id)) { // Cartographer
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114184685420615/IH_PatreonTierImages_Cartographer_.jpg');
-	} else if (archivists.includes(receivedMessage.author.id)) { // Archivist
+	} else if (patrons.archivists.includes(receivedMessage.author.id)) { // Archivist
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114183620067358/IH_PatreonTierImages_Archivist.jpg');
-	} else if (grandArchivists.includes(receivedMessage.author.id)) { // Grand Archivist
+	} else if (patrons.grandArchivists.includes(receivedMessage.author.id)) { // Grand Archivist
 		embed.setImage('https://cdn.discordapp.com/attachments/545684759276421120/734114189710327818/IH_PatreonTierImages_Grand_Archivist.jpg');
 	}
 
